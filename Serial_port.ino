d6e1150ac05ef30a77f48c1210c4c60070eda5b8
1z-cm <3172345331@qq.com>
2025-07-28 10:42:20 +0800

    Update Serial_port.ino

--- Serial_port.ino
+++ Serial_port.ino
@@ -3,7 +3,7 @@ void setup() {
   while (!Serial);          // 等待串口连接（仅需在部分开发板启用）
   Serial.println("www.sparkleiot.com");  // 打印目标内容并换行
   Serial.println("github:https://github.com/Sparkleiot/arduino_sdk.git");
-  Serial.println("Arduino:https://github.com/tuya/arduino-tuyaopen/releases/download/global/package_tuya_open_index.json");
+  Serial.println("Arduino:https://github.com/tuya/arduino-TuyaOpen");
 }
 
 void loop() {
